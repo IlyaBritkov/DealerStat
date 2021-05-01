eba970a89d1b17e9814781d67a2a4ea5cb114076
Ilya <britkov.ilya.a@gmail.com>
2021-05-02 01:45:34 +0300

    Add FeedbackService

---
+++ src/main/java/com/leverx/service/FeedbackService.java
@@ -0,0 +1,20 @@
+package com.leverx.service;
+
+import com.leverx.dto.FeedbackDTO;
+import com.leverx.exception_handling.exception.NoSuchEntityException;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FeedbackService {
+
+    List<FeedbackDTO.Response.Public> findAll();
+
+    Optional<FeedbackDTO.Response.Public> findById(Integer id) throws RuntimeException;
+
+    FeedbackDTO.Response.Public save(FeedbackDTO.Request.Create feedbackDtoRequest) throws NoSuchEntityException;
+
+    FeedbackDTO.Response.Public update(FeedbackDTO.Request.Update feedbackDtoRequest) throws NoSuchEntityException;
+
+    void deleteById(Integer id) throws RuntimeException;
+}
